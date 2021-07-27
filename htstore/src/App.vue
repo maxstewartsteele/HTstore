a0743e982c1d99f2d21e80bf3327a5e91321a6aa
maxstewartsteele <66533762+maxstewartsteele@users.noreply.github.com>
2021-07-27 16:05:44 -0400

    Add files via upload

---
+++ htstore/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div id="Router">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+//import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+ 
